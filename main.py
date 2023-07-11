d06e67c0aec1386ea449039cabe296b3c89acaca
chasing444 <chae_sp@outlook.com>
2023-07-11 10:49:17 +0700

    test
    test

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from urllib.parse import quote
 import pandas as pd
 from urllib.parse import quote
 
-dbschema = 'database_api_cdp'
-password = 'toto@2021'
+dbschema = "database_api_cdp"
+password = "toto@2021"
 encoded_password = quote(password)
 
 engine = create_engine(
-    f'mysql+mysqlconnector://toto:{encoded_password}@35.240.144.191:3306/{dbschema}'
+    f"mysql+mysqlconnector://toto:{encoded_password}@35.240.144.191:3306/{dbschema}"
 )
 
 query = """select * from database_api_cdp.configs where deleted_at is null"""
 datas = pd.read_sql(query, con=engine)
 
 
+# hi
